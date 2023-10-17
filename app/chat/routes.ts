bb28b449d47e72a698d77f252b85b0d619e537ea
Winnie <winnieng3210@gmail.com>
2023-10-17 17:32:10 -0400

    initial commit plus setting up openAI

---
+++ app/chat/routes.ts
@@ -0,0 +1,33 @@
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+// Set the runtime to edge for best performance
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  // WINNIE'S TODO: CHANGE VIBE TO SOMETHING ELSE
+  const { vibe, bio } = await req.json();
+
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: [
+      {
+        role: "user",
+        content: `Give me a list of 5 cocktail drinks that I can make with some or all of these ingredients: lemon, grenadine, rum`,
+      },
+    ],
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
